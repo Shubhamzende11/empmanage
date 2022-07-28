483c9079ec8a45b319e3363f5abfa0b5add0253f
Monika <monikagavate5@gmail.com>
2022-07-28 14:34:48 +0530

    commit code

--- empmanageapp/urls.py
+++ empmanageapp/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
 
     path('', views.index,name='index'),
     path('registration',views.registration,name='registration'),
-    path('emp_login', views.emp_login, name='emp_login'),
-    path('emp_home', views.emp_home, name='emp_home'),
+    path('/emp_login', views.emp_login, name='emp_login'),
+    path('/emp_home', views.emp_home, name='emp_home'),
     path('profile', views.profile, name='profile'),
     path('Logout', views.Logout, name='logout'),
     path('admin_login', views.admin_login, name='admin_login'),
