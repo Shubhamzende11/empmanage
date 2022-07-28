cac37c2f0fbe5afc408550138d2a91280a532c47
Monika <monikagavate5@gmail.com>
2022-07-28 12:41:02 +0530

    code

--- empmanageproj/settings.py
+++ empmanageproj/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
-STATISTICS_DIRS=(
-     os.path.join(BASE_DIR,'static'),
- )
+# STATISTICS_DIRS=(
+#      os.path.join(BASE_DIR,'static'),
+#  )
 STATIC_ROOT=BASE_DIR / 'static'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
