4029971b9b59397eb7ecce9f37c04ee08c134c60
Monika <monikagavate5@gmail.com>
2022-07-28 18:25:53 +0530

    commit code

--- empmanageproj/settings.py
+++ empmanageproj/settings.py
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 #      os.path.join(BASE_DIR,'static'),
 #  )
 STATIC_ROOT=BASE_DIR, 'static'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
