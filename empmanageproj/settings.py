e8303a8bd46aaf2ad317060788bf34f8837b6598
Monika <monikagavate5@gmail.com>
2022-07-28 17:23:27 +0530

    commit code

--- empmanageproj/settings.py
+++ empmanageproj/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-a)=70y4sb@_p4x)t^mt3y^(q$)gn)ye&&a__7h8425)$t$70$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
