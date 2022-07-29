2cdba8dfff31fb832b98718c24eaeee98887204b
Monika <monikagavate5@gmail.com>
2022-07-29 12:03:28 +0530

    code commit

--- empmanageapp/urls.py
+++ empmanageapp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('index', views.index,name='index'),
+    path('/index', views.index,name='index'),
     path('registration',views.registration,name='registration'),
     path('emp_login', views.emp_login, name='emp_login'),
     path('emp_home', views.emp_home, name='emp_home'),
